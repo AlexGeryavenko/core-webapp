837bf6660d77ca14f83981761d61813d30fafb8d
oleksandr.geryavenko <a.oleksandr.geryavenko@ericsson.com>
2018-07-30 13:24:35 +0300

    Init commit

---
+++ src/main/java/my/example/core/webapp/corewebapp/CoreWebappApplication.java
@@ -0,0 +1,12 @@
+package my.example.core.webapp.corewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreWebappApplication.class, args);
+	}
+}
